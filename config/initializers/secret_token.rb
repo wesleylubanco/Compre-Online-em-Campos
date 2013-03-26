e12aa6dcb828c602e2a443f1387c506e949ead23
lab-cord <lab-cord@ama.(none)>
2013-03-26 11:39:18 -0300

    Inicio do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Compreonline::Application.config.secret_token = 'e76e2aad71b9318eb064f7e5a236708deaa8e3908a81bca01484956667cdcac8d8599ba44d9f3b1c2d42f2ec76f491f7111f6b2778a89ac780fce794584da8af'
